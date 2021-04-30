67bdad55b04435af7d2f298466ab60c4016112ff
Arunima Mandal <arunima1590@gmail.com>
2021-04-30 18:08:45 -0400

    Update settings.py

--- protein_design/settings.py
+++ protein_design/settings.py
@@ -148,7 +148,7 @@ EMAIL_USE_TLS = False
 EMAIL_HOST = 'smtp.gmail.com' 
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = 'arunima1590@gmail.com'
-EMAIL_HOST_PASSWORD = 'loujdswnocomqdgz'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
